823a8151674e6f700bf38944959a8113da873963
santiago zaidan <santyagozaidan@gmail.com>
2023-06-23 16:28:38 -0300

    functionality added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,71 +1,10 @@
-import { useState, useEffect } from 'react'
+import DropComponent from "./DropComponent"
 
 function App() {
-  const [hasImage, setHasImage] = useState(false)
-  const [imageSrc, setImageSrc] = useState('')
-
-  const eventTypes: string[] = ['dragenter', 'dragover', 'dragleave', 'drop']
-
-  const preventDefaults = (e: any) => {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-
-  useEffect(() => {
-    const dropArea: HTMLElement = document.getElementById('drop-area')!
-    const imageContainer: HTMLElement = document.getElementById('image-container')!
-
-    const handleImg = (e: any) => {
-      if (hasImage) return
-      const file = e.dataTransfer.files[0]
-      console.log(file)
-      if (file && file.type.startsWith('image/')) {
-        const imageURL = URL.createObjectURL(file)
-        const imageElement = document.createElement('img')
-        imageElement.src = imageURL
-        imageElement.style.height = '100px'
-        imageContainer.appendChild(imageElement)
-        setImageSrc(imageURL)
-        setHasImage(true)
-      }
-    }
-
-    const copyImage = () => {
-      if (hasImage) {
-        navigator.clipboard.write([
-          new ClipboardItem({ 'image/png': imageSrc })
-        ])
-      }
-    }
-
-    eventTypes.forEach(evn => dropArea.addEventListener(evn, preventDefaults))
-    dropArea.addEventListener('drop', handleImg)
-
-    return () => {
-      eventTypes.forEach(evn => dropArea.removeEventListener(evn, preventDefaults))
-      dropArea.removeEventListener('drop', handleImg)
-    }
-  }, [hasImage]);
 
   return (
     <>
-      <div id="drop-area" style={{ width: '500px', height: '500px', backgroundColor: 'gray' }}>
-        <form style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-          <p>Upload a file</p>
-          <input type="file" id="file" accept="image/*"></input>
-          {
-            hasImage ? (
-              <>
-                <button id='copy-button' style={{ visibility: 'hidden' }}>copy image</button>
-              </>
-            )
-              : (
-                <p>...or drop an image here</p>
-              )
-          }
-          <div id='image-container'></div>
-        </form>
-      </div>
+      <DropComponent />
     </>
   )
 }
--- client/src/DropComponent.tsx
+++ client/src/DropComponent.tsx
@@ -1,40 +1,87 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import illustration from './assets/image.svg'
+import './app.css'
 
 const DropComponent = () => {
     const [hasImage, setHasImage] = useState(false)
     const [imageSrc, setImageSrc] = useState('')
 
-    const handleDrop = (e: any) => {
-        e.preventDefault()
-        e.stopPropagation()
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
+    const handleFileButtonClick = () => {
+        if(fileInputRef.current) fileInputRef.current.click()
     }
 
-    const copyImage = () => {
+    const preventDefaults = (e: any) => {
+        e.preventDefault()
+    }
 
+    const handleImg = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+        if (hasImage) return
+        let file
+        if (e.type == 'drop') {
+            file = e.dataTransfer.files[0]
+        } else {
+            file = e.target.files[0]
+        }
+        if (file && file.type.startsWith('image/')) {
+            const imageURL = URL.createObjectURL(file)
+            setImageSrc(imageURL)
+            setHasImage(true)
+        }
+    }
+
+    useEffect(() => {
+        const eventTypes: string[] = ['dragenter', 'dragover', 'dragleave', 'drop']
+        const dropArea: HTMLElement = document.getElementById('drop-area')!
+
+        eventTypes.forEach(evn => dropArea.addEventListener(evn, preventDefaults))
+
+        return () => {
+            eventTypes.forEach(evn => dropArea.removeEventListener(evn, preventDefaults))
+        }
+    }, [hasImage])
+
+    const copyImage = () => {
+        if (hasImage) {
+            navigator.clipboard.writeText(imageSrc)
+        }
     }
 
     return (
-        <>
-            <div onDrop={handleDrop} 
-            onDragEnter={e => e.preventDefault()} 
-            onDragOver={e => e.preventDefault()}>
-                {
-                    hasImage ?
-                    (
-                        <>
-                            <img src={imageSrc} alt='Dropped image' />
-                            <button onClick={copyImage}>Copy image to clipboard</button>
-                        </>
-                    )
-                    :
-                    (
-                        <>
-                            <input type='file' />
-                        </>
-                    )
-                }
+        <div id='container-all'>
+            <div id='shadow'>
+                <div id='container'>
+                    <h4>Upload your image</h4>
+                    <span>File should be jpeg, png...</span>
+                    <div id='drop-area' onDrop={handleImg}>
+                        {
+                            hasImage ?
+                                (
+                                    <div id='container-img'>
+                                        <img src={imageSrc} alt='Selected image' id='img-selected' />
+                                        <button onClick={copyImage}>Copy image to clipboard</button>
+                                    </div>
+                                )
+                                :
+                                (
+                                    <>
+                                        <img id='illustration' src={illustration} />
+                                        <p>Drag & Drop your image here</p>
+                                    </>
+                                )
+                        }
+                    </div>
+                </div>
+                <>
+                    <p>Or</p>
+                    <input type='file' ref={fileInputRef} style={{ display: 'none' }} onChange={handleImg} accept='.jpeg, .jpg, .png' />
+                    <button id='button-file' onClick={handleFileButtonClick}>Choose a file</button>
+                </>
             </div>
-        </>
+        </div>
     )
 }
 
